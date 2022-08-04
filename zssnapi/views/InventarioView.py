122e2a21a4d6b3a7bae20de207de1a85deeab02c
eslleyrubem <rubemeslley@gmail.com>
2022-08-04 03:18:10 -0300

    Verificando se o sobrevivente esta infectado ao acessar inventario

--- zssnapi/views/InventarioView.py
+++ zssnapi/views/InventarioView.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from zssnapi.models import InventarioModel
+from zssnapi.models.SobreviventeModel import SobreviventeModel
 from zssnapi.serializers import InventarioSerializer
 
 @api_view(['GET'])
@@ -13,9 +14,13 @@ def inventariosList(request):
 
 @api_view(['GET'])
 def inventarioDetail(request, fk):
-    inventarios = InventarioModel.objects.filter(sobrevivente=fk).values('item', 'quantidade')
+    isInfectado = SobreviventeModel.objects.get(id=fk).estaInfectado
+    if isInfectado:
+        return Response("Não é possível acessar inventário, sobrevivente infectado", status=status.HTTP_200_OK)
+    else:
+        inventarios = InventarioModel.objects.filter(sobrevivente=fk).values('item', 'quantidade')
     
-    return Response(inventarios, status=status.HTTP_200_OK)
+        return Response(inventarios, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def inventarioCreate(request):
