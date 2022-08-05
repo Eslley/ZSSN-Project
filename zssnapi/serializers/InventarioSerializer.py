760c79d72bb3c67e3edac07d6af85c9237a88c28
eslleyrubem <rubemeslley@gmail.com>
2022-08-04 22:35:05 -0300

    fix: Declarando inventario no cadastro do sobrevivente

--- zssnapi/serializers/InventarioSerializer.py
+++ zssnapi/serializers/InventarioSerializer.py
@@ -1,14 +1,18 @@
 from rest_framework import serializers
 from zssnapi.models import InventarioModel
+from zssnapi.models.SobreviventeModel import SobreviventeModel
 from zssnapi.serializers.ItemSerializer import ItemSerializer
 from zssnapi.serializers.SobreviventeSerializer import SobreviventeSerializer
 
 class InventarioSerializer(serializers.ModelSerializer):
 
-    item = ItemSerializer()
-    sobrevivente = serializers.StringRelatedField()
-
     class Meta:
 
         model = InventarioModel
         exclude = ['id']
+
+    def to_representation(self, instance):
+        return {
+            'sobrevivente': SobreviventeSerializer(instance.sobrevivente).data['nome'],
+            'item': ItemSerializer(instance.item).data
+        }
