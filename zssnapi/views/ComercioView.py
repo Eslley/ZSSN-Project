1f2f0f95e2125f5ff112afe3b68332c377cb40cf
eslleyrubem <rubemeslley@gmail.com>
2022-08-05 21:59:37 -0300

    Controlando commit e rollback caso a transação de trocas falhe

--- zssnapi/views/ComercioView.py
+++ zssnapi/views/ComercioView.py
@@ -7,6 +7,7 @@ from zssnapi.models.SobreviventeModel import SobreviventeModel
 from zssnapi.serializers.ComercioSerializer import ComercioSerializer
 from django.db import transaction
 from django.db.models import F
+from django.core.exceptions import EmptyResultSet
  
 @api_view(['POST'])
 def negociar(request):
@@ -20,19 +21,28 @@ def negociar(request):
         itens1 = serializer.data.get('sobrevivente1').get('itens')
         itens2 = serializer.data.get('sobrevivente2').get('itens')
         
-        retorno = retirarItens(sId1, itens1)
-        if not isinstance(retorno, bool):
-            return retorno
-        retorno = retirarItens(sId2, itens2)
-        if not isinstance(retorno, bool):
-            return retorno
+        transaction.set_autocommit(False)
+        try:
+            retirarItens(sId1, itens1)
+            retirarItens(sId2, itens2)
 
-        adicionarItens(sId1, itens2)
-        adicionarItens(sId2, itens1)
+            adicionarItens(sId1, itens2)
+            adicionarItens(sId2, itens1)
+        except EmptyResultSet:
+            transaction.rollback()
+            return Response({'message': 'Item oferecido não está no inventário do sobrevivente'}, status=status.HTTP_200_OK)
+        except:
+            transaction.rollback()
+        else:
+            transaction.commit()
+            return Response({'message': 'Troca realizada com sucesso'}, status=status.HTTP_200_OK)
+        finally:
+            transaction.set_autocommit(True)
+
     else:
         return Response(serializer.errors, status=status.HTTP_200_OK)
 
-    return Response({'message': 'Troca realizada com sucesso'}, status=status.HTTP_200_OK)
+    
 
 def retirarItens(sId, itens):
     for item in itens:
@@ -45,7 +55,7 @@ def retirarItens(sId, itens):
                 inventario.update(quantidade=F('quantidade') - item.get('quantidade'))
 
         else:
-            return Response({'message': 'Item oferecido não está no inventário do sobrevivente'}, status=status.HTTP_200_OK)
+            raise EmptyResultSet()
     
     return True
 
