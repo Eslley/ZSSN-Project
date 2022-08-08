c92baac84b35a1a74d4d37abd51b5571bacefc48
eslleyrubem <rubemeslley@gmail.com>
2022-08-08 20:45:20 -0300

    Implementando testes unitarios

---
+++ zssnapi/tests/test_setup.py
@@ -0,0 +1,49 @@
+from django.urls import reverse
+from rest_framework.test import APITestCase
+
+from zssnapi.models.SobreviventeModel import SobreviventeModel
+
+class TestSetUp(APITestCase):
+
+    def setUp(self):
+
+        self.sobrevivente = SobreviventeModel.objects.create(nome='Nanfred Junior', idade=22, sexo='m', latitude=5.0888877, longitude=42.999991)
+        self.sobrevivente2 = SobreviventeModel.objects.create(nome='Dubov Francisco', idade=32, sexo='m', latitude=5.665334, longitude=42.0021333)
+        self.sobrevivente3 = SobreviventeModel.objects.create(nome='Joanne Fay', idade=28, sexo='f', latitude=5.000022, longitude=42.0021113)
+        self.sobrevivente4 = SobreviventeModel.objects.create(nome='Marsha', idade=42, sexo='f', latitude=5.885334, longitude=41.0023300)
+
+        self.create_url = reverse('sobrevivente-create')
+        self.update_localization_url = reverse('sobrevivente-update-localization', kwargs={'pk': self.sobrevivente.pk})
+        self.alert_contamination_url = reverse('sobrevivente-infectado', kwargs={'info': self.sobrevivente.pk, 'cont': self.sobrevivente2.pk})
+
+        self.sobrevivente_dados_sem_inventario = {
+            "nome": "Rubem Eslley",
+            "idade": 20,
+            "sexo": "m",
+            "latitude": 5.037178,
+            "longitude": 42.792717
+        }
+
+        self.sobrevivente_dados_corretos = {
+            "nome": "Rubem Eslley",
+            "idade": 20,
+            "sexo": "m",
+            "latitude": 5.037178,
+            "longitude": 42.792717,
+            "inventario": [
+                {
+                    "id": 4,
+                    "quantidade": 2
+                    
+                },
+                {
+                    "id": 2,
+                    "quantidade": 2
+                }
+            ]
+        }
+
+        return super().setUp()
+
+    def tearDown(self):
+        return super().tearDown()
---
+++ zssnapi/tests/test_views.py
@@ -0,0 +1,74 @@
+from django.urls import reverse
+from urllib import response
+from zssnapi.models.InventarioModel import InventarioModel
+from zssnapi.tests.test_setup import TestSetUp
+
+
+class TestViews(TestSetUp):
+
+    def test_sobrevivente_nao_pode_registrar_sem_dados(self):
+        response = self.client.post(self.create_url)
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_sobrevivente_nao_pode_registar_sem_inventario(self):
+        response = self.client.post(self.create_url, self.sobrevivente_dados_sem_inventario)
+        
+        self.assertEqual(response.status_code, 400)
+
+    def test_sobrevivente_cadastrar_corretamente(self):
+        response = self.client.post(self.create_url, self.sobrevivente_dados_corretos, format='json')
+        
+        self.assertEqual(response.status_code, 201)
+
+    def test_atualiza_localizacao_sobrevivente_corretamente(self):
+        sobrevivente_localization = {
+            "latitude": 5.1234567,
+            "longitude": 42.1234567,
+        }
+
+        response = self.client.put(self.update_localization_url, sobrevivente_localization, format='json')
+
+        self.sobrevivente.refresh_from_db()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.sobrevivente.latitude, 5.1234567)
+        self.assertEqual(self.sobrevivente.longitude, 42.1234567)  
+
+    def test_atualiza_localizacao_sobrevivente_sem_dados(self):
+        sobrevivente_localization = {}
+
+        response = self.client.put(self.update_localization_url, sobrevivente_localization, format='json')
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_contabiliza_uma_sinalizacao_de_contaminacao(self):
+        response = self.client.put(self.alert_contamination_url)
+
+        self.sobrevivente2.refresh_from_db()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.sobrevivente2.countAlertInfected, 1)
+        self.assertEqual(self.sobrevivente2.estaInfectado, False)
+
+    def test_contabiliza_tres_sinalizacoes_de_contaminacao(self):
+
+        self.alert_contamination_url = reverse('sobrevivente-infectado', kwargs={'info': self.sobrevivente.pk, 'cont': self.sobrevivente4.pk})
+        response = self.client.put(self.alert_contamination_url)
+
+        self.assertEqual(response.status_code, 200)
+
+        self.alert_contamination_url = reverse('sobrevivente-infectado', kwargs={'info': self.sobrevivente2.pk, 'cont': self.sobrevivente4.pk})
+        response = self.client.put(self.alert_contamination_url)
+
+        self.assertEqual(response.status_code, 200)
+
+        self.alert_contamination_url = reverse('sobrevivente-infectado', kwargs={'info': self.sobrevivente3.pk, 'cont': self.sobrevivente4.pk})
+        response = self.client.put(self.alert_contamination_url)
+
+        self.assertEqual(response.status_code, 200)
+
+        self.sobrevivente4.refresh_from_db()
+
+        self.assertEqual(self.sobrevivente4.countAlertInfected, 3)
+        self.assertEqual(self.sobrevivente4.estaInfectado, True)
