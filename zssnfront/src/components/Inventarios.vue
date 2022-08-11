3901fd263705123e164b498fa5363dc9a0494f48
eslleyrubem <rubemeslley@gmail.com>
2022-08-11 14:22:39 -0300

    Exibindo inventarios

--- zssnfront/src/components/Inventarios.vue
+++ zssnfront/src/components/Inventarios.vue
@@ -1,9 +1,78 @@
 <template>
-    <div>
-        Inventarios
-    </div>
+
+  <div class="container">
+      
+    <h4 class="center-align">Inventários</h4>
+
+    <table class="center">
+
+      <thead>
+
+        <tr>
+          <th>Sobrevivente</th>
+          <th class="center-align">Inventário</th>
+        </tr>
+
+      </thead>
+
+      <tbody>
+
+        <tr v-for="inventario in inventarios" :key="inventario.sobreviventeId">
+
+          <td>{{ inventario.sobrevivente }}</td>
+
+          <td class="center-align">
+            <div class="collection">
+              <a href="#!" v-for="item in inventario.itens" :key="item.index" class="collection-item">
+                {{ item.item.nome }}<span class="badge">{{ item.quantidade }}</span>
+              </a>
+            </div>
+          </td>
+
+        </tr>
+
+      </tbody>
+    
+    </table>
+
+  </div>
 </template>
 
 <script>
 
+    import Inventarios from '../services/inventarios'
+
+    export default {
+        name: "Inventarios",
+
+        data() {
+          return {
+              inventarios: []
+          }
+        },
+
+        mounted() {
+          this.getInventarios()
+        },
+
+        methods: {
+          getInventarios() {
+              Inventarios.listar().then(response => {
+                  this.inventarios = response.data.inventarios
+                  console.log(this.inventarios)
+              }).catch(err => {
+
+              })
+          }
+        }
+    }
+
 </script>
+
+<style scoped>
+
+  .collection {
+    border: none;
+  }
+
+</style>
